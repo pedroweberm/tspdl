999b684f7b3872b7078baa41774da99556c7edbc
Pedro Weber <pedroweberm@gmail.com>
2019-06-29 13:45:56 -0300

    inicio da geracao inicial de individuos

--- genetic.py
+++ genetic.py
@@ -4,16 +4,43 @@ import numpy as np
 import time
 import sys
 
+costs = [[]]
+demands = []
+drafts = []
+n = 5
+seed = mktime()
+
+
 def parse_file(fileName):
     with open(fileName, "r") as f:
         lines = f.readlines()
-        for line in lines: 
+        for line in lines:
             line = line.replace("\n", "")
             print(line)
 
-def generate_initial_pop(initialPop):
+
+def generate_initial_pop(initialPop, popSize):
+    for i in range(0, popSize - 1):
+        initialPop.append(generateInidividual())
     pass
-    #return bestIndividual
+    # return bestIndividual
+
+
+def generateIndividual():
+    global costs
+    global demands
+    global drafts
+    global n
+    global seed
+
+    random.seed(seed)
+
+    currentDraft = sum(drafts)
+    validChoices = []
+
+    for i in range(0, n - 1):
+        if drafts[i] <= currentDraft:
+            validChoices.append(i)
 
 
 def evaluate_pop(pop, costs):
@@ -24,39 +51,44 @@ def evaluate_pop(pop, costs):
         totalCost = totalCost + individualCost
         popCosts.append(individualCost)
 
-    averageCost = totalCost/pop.len()
+    averageCost = totalCost/len(pop)
+
 
 def evaluate(c, x):
     cost = 0
-    for i in range(0, x.len()):
-        for j in range(0, c.len()):
+    for i in range(0, len(x)):
+        for j in range(0, len(c)):
             cost = cost + x[i][j]*c[i][j]
 
     return cost
 
+
 def select_individuals():
     pass
+
 
 def reproduce():
     pass
 
+
 def mutate():
     pass
 
+
 def genetic_alg(initialPop):
     pass
-    #return bestIndividual
+    # return bestIndividual
 
 
 def generate_neighbour():
     pass
+
 
 if __name__ == '__main__':
     parse_file("instances/bayg29_10_1.dat")
     totalInitialCosts = 0
     totalFinalCosts = 0
     initialPop = []
-    costs = []
     for i in range(0, 9):
         initialCost = evaluate(costs, generate_initial_pop(initialPop)["x"])
         totalInitialCosts = totalInitialCosts + initialCost
