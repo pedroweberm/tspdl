b30716ae616d88b043ec2ce88a5515787b46706d
Pedro Weber <pedroweberm@gmail.com>
2019-06-29 18:03:23 -0300

    Funcao para testar validade da solucao

--- genetic.py
+++ genetic.py
@@ -6,7 +6,7 @@ import sys
 
 costs = [[1,2,3,4,5],[5,4,3,2,1],[1,3,2,4,5],[5,2,1,3,4],[3,5,2,4,1],[1,2,3,4,5],[5,4,3,2,1],[1,3,2,4,5],[5,2,1,3,4],[3,5,2,4,1]]
 demands = [0,1,1,1,1,1,1,1,1,1]
-drafts = [9,1,9,9,9,9,9,9,9,9]
+drafts = [9,1,9,9,9,8,9,9,2,9]
 n = 10
 seed = 5
 
@@ -70,7 +70,76 @@ def parse_file(fileName):
         # print(demands)
         # print(drafts)
 
+def validateSolution(x, y):
 
+    global demands
+    global costs
+    global n
+    global drafts
+    
+    rest1 = 1;
+    #Primeiro conjunto de restricoes: soma de todas colunas e igual a 1
+    for j in range(0, n):
+        rest1 = 0
+        for i in range(0, n):
+            rest1 += x[i][j]
+        if (rest1 != 1):
+            print("SOLUÇÃO NÃO É VÁLIDA: Soma alguma coluna não é igual a 1")
+            return False
+    
+    rest2 = 1;
+    #Segund0 conjunto de restricoes: soma de todas linhas e igual a 1
+    for i in range(0, n):
+        rest2 = 0
+        for j in range(0, n):
+            rest2 += x[i][j]
+        if (rest2 != 1):
+            print("SOLUÇÃO NÃO É VÁLIDA: Soma de alguma linha não é igual a 1")
+            return False
+
+    rest31 = 0
+    rest32 = 0
+    #Terceiro conjunto de restricoes: demanda dos portos e atendidas
+    for j in range(1, n):
+        rest31 = 0
+        rest32 = 0
+        for i in range(0, n):
+            rest31 += y[i][j]
+        for i in range(0, n):
+            rest32 += y[j][i]
+        if ((rest31 - rest32) != demands[j]):
+            print("SOLUÇÃO NÃO É VÁLIDA: Demanda de todos portos não é atendida")
+            return False
+    
+    rest41 = 0
+    rest42 = 0
+    #Quarto conjunto de restricoes: embarcacao sai da garagem carregada com toda demanda necessaria
+    for j in range(0, n):
+        rest41 += y[0][j]
+    for i in range(0, n):
+        rest42 += demands[i]
+    
+    if rest41 != rest42:
+        print("SOLUÇÃO NÃO É VÁLIDA: Embarcação não sai suficientemente carregada (soma das demandas)")
+        return false
+
+    rest5 = 0
+    #Quinto conjunto de restricoes: so retorna para o porto quando estiver vazio
+    for i in range(0, n):
+        rest5 += y[i][0]
+
+    if rest5 != 0:
+        print("SOLUÇÃO NÃO É VÁLIDA: Embarcação retorna para o porto com carga maior que 0")
+        return False
+
+    #Sexto conjunto de restricoes: limites de profundidade e implicacao
+    for i in range(0, n):
+        for j in range(0, n):
+            if (y[i][j] < 0 || y[i][j] > drafts[j]*x[i][j]):
+                print("SOLUÇÃO NÃO É VÁLIDA: Profundidade limite violada ou implicação de y e x violada")
+                return False
+
+    
 def generate_initial_pop(initialPop, popSize):
     for i in range(0, popSize - 1):
         initialPop.append(generateIndividual())
@@ -101,15 +170,11 @@ def generateIndividual():
                 validChoices.append(unvisited[j])
 
         chosen = random.choice(validChoices)
-        print(chosen)
-        print("removendo ", chosen, " de ", unvisited, " na posicao ", chosen)
         currentDraft = currentDraft - demands[chosen]
         unvisited.remove(chosen)
         solucao.append(chosen)
-        
 
     print(solucao)
-    
 
 
 def evaluate_pop(pop, costs):
@@ -154,10 +219,10 @@ def generate_neighbour():
 
 
 if __name__ == '__main__':
-    # parse_file(sys.argv[1])
-    #totalInitialCosts = 0
-    #totalFinalCosts = 0
-    #initialPop = []
+    parse_file(sys.argv[1])
+    totalInitialCosts = 0
+    totalFinalCosts = 0
+    initialPop = []
     global costs
     global demands
     global drafts
