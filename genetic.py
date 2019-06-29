e42bff425f3bc48f55157e5e2ce8ed94e022b7d5
Gustavo Lorentz <gus.aca.lo@gmail.com>
2019-06-29 18:17:34 -0300

    Create Solution adn Validate

--- genetic.py
+++ genetic.py
@@ -8,7 +8,7 @@ costs = [[1,2,3,4,5],[5,4,3,2,1],[1,3,2,4,5],[5,2,1,3,4],[3,5,2,4,1],[1,2,3,4,5]
 demands = [0,1,1,1,1,1,1,1,1,1]
 drafts = [9,1,9,9,9,9,9,9,9,9]
 n = 10
-seed = 5
+seed = 50
 
 
 def parse_file(fileName):
@@ -88,29 +88,42 @@ def generateIndividual():
     #random.seed(seed)
 
     x = [[0] * int(n) for i in range(int(n))]
-    unvisited = range(1, n)
-    validChoices = []
-    solucao = []
-
-    currentDraft = sum(demands)
+    y = [[0] * int(n) for i in range(int(n))]
     
-    for i in  range(0, n-1):
-        validChoices = []
-        for j in range(0, len(unvisited)):
-            if drafts[unvisited[j]] >= currentDraft:
-                validChoices.append(unvisited[j])
+    solucaoValida = False
+    while(not solucaoValida):
+        currentDraft = sum(demands)
+        unvisited = list(range(1, n)) 
+        solucao = []
+        for i in  range(0, n-1):
+            validChoices = []
+            for j in range(0, len(unvisited)):
+                if drafts[unvisited[j]] >= currentDraft:
+                    validChoices.append(unvisited[j])
 
-        chosen = random.choice(validChoices)
-        print(chosen)
-        print("removendo ", chosen, " de ", unvisited, " na posicao ", chosen)
-        currentDraft = currentDraft - demands[chosen]
-        unvisited.remove(chosen)
-        solucao.append(chosen)
-        
-
+            chosen = random.choice(validChoices)
+            currentDraft = currentDraft - demands[chosen]
+            unvisited.remove(chosen)
+            solucao.append(chosen)
+        solucaoValida = validateSolution(solucao)
+    
     print(solucao)
-    
+    currentDraft = sum(demands) - demands[solucao[0]]
+    for i in range(len(solucao)):
+        if i >= 0 and i < len(solucao) - 1:
+            x[solucao[i]][solucao[int(i)+1]] = 1
+            y[solucao[i]][solucao[int(i)+1]] = currentDraft
+        if i == 0:
+            x[0][solucao[i]] = 1
+            y[0][solucao[i]] = sum(demands)
+        if i == len(solucao) - 1:
+            x[solucao[i]][0] = 1
+            y[solucao[i]][0] = currentDraft
 
+        currentDraft = currentDraft - demands[solucao[i]]
+
+def validateSolution(solution):
+    return True
 
 def evaluate_pop(pop, costs):
     totalCost = 0
@@ -154,14 +167,10 @@ def generate_neighbour():
 
 
 if __name__ == '__main__':
-    # parse_file(sys.argv[1])
+    #parse_file(sys.argv[1])
     #totalInitialCosts = 0
     #totalFinalCosts = 0
     #initialPop = []
-    global costs
-    global demands
-    global drafts
-    global n
 
     #for i in range(0, 9):
     #    initialCost = evaluate(costs, generate_initial_pop(initialPop)["x"])
