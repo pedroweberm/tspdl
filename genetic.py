3f5a0e1334a700e8dea73766c2e4ba9f77096774
Gustavo Lorentz <gus.aca.lo@gmail.com>
2019-06-29 11:30:24 -0300

    Funcoes

---
+++ genetic.py
@@ -0,0 +1,22 @@
+from controller_interface import ControllerInterface
+import random
+import math
+import numpy as np
+import time
+        
+def generate_initial_pop(self):
+
+def evaluate_pop(self):
+
+def select_individuals(self):
+
+def reproduce(self):
+
+def mutate(self):
+
+def genetic_alg(self:
+
+def generate_neighbour(self):
+
+def learn(self):
+
