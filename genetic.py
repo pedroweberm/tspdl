5abcaa585d45d5fd2c1d1b99a24b22a89b2c303f
Pedro Weber <pedroweberm@gmail.com>
2019-06-29 12:27:08 -0300

    base para implementacao

--- genetic.py
+++ genetic.py
@@ -1,22 +1,61 @@
-from controller_interface import ControllerInterface
 import random
 import math
 import numpy as np
 import time
-        
-def generate_initial_pop(self):
+import sys
 
-def evaluate_pop(self):
 
-def select_individuals(self):
+def generate_initial_pop(initialPop):
 
-def reproduce(self):
+    return bestIndividual
 
-def mutate(self):
 
-def genetic_alg(self:
+def evaluate_pop(pop, costs):
+    totalCost = 0
+    popCosts = []
+    for individual in pop:
+        individualCost = evaluate(costs, individual["x"])
+        totalCost = totalCost + individualCost
+        popCosts.append(individualCost)
 
-def generate_neighbour(self):
+    averageCost = totalCost/pop.len()
 
-def learn(self):
 
+def evaluate(c, x):
+    cost = 0
+    for i in range(0, x.len()):
+        for j in range(0, c.len()):
+            cost = cost + x[i][j]*c[i][j]
+
+    return cost
+
+
+def select_individuals():
+
+
+def reproduce():
+
+
+def mutate():
+
+
+def genetic_alg(initialPop):
+
+    return bestIndividual
+
+
+def generate_neighbour():
+
+
+def learn():
+    totalInitialCosts = 0
+    totalFinalCosts = 0
+    initialPop = []
+    for i in range(0, 9):
+        initialCost = evaluate(costs, generate_initial_pop(initialPop)["x"])
+        totalInitialCosts = totalInitialCosts + initialCost
+        finalCost = evaluate(costs, genetic_alg(initialPop)["x"])
+        totalFinalCosts = totalFinalCosts + finalCost
+
+    averageInitialCost = totalInitialCosts/initialPop.len()
+    averageFinalCost = totalFinalCosts/initialPop.len()
